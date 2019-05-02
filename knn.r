b458fe29dc213eeb78f324a13cbb0fa12fb2e83c
OmarTahoun <omaraiz489@gmail.com>
2019-05-02 18:09:48 +0800

    update

--- knn.r
+++ knn.r
@@ -1,8 +1,9 @@
 dataset <- read.csv("data.csv")
-new_point <- c(5,7)
+new_point <- read.csv("test.csv")
 
 
-KNN = function(x,y,k=5)
+###Nearest neighbors
+my_knn_regressor = function(x,y,k=3)
 {
   if (!is.matrix(x))
   {
@@ -22,25 +23,40 @@ KNN = function(x,y,k=5)
 
 compute_pairwise_distance=function(X,Y)
 {
-  print(X)
-  print(Y)
   xn = rowSums(X ** 2)
   yn = rowSums(Y ** 2)
   outer(xn, yn, '+') - 2 * tcrossprod(X, Y)
 }
 
-
 predict.my_knn_regressor = function(my_knn,x)
 {
-  print(x)
   if (!is.matrix(x))
   {
-    x <- matrix(x, nrow=1, ncol = 2)
+    x = as.matrix(x)
   }
-  ## Compute pairwise distance
+  ##Compute pairwise distance
   dist_pair = compute_pairwise_distance(x,my_knn[['points']])
-  print(dist_pair)
-  ## orders the points by distance and select the k-closest points
-  apply(dist_pair,1,order) <= my_knn[['k']], my_knn[["value"]]
-  crossprod() / my_knn[['k']]
+
+  dist_pair <- t(as.matrix(apply(dist_pair,1,order) <= my_knn[['k']]))
+  my_knn[["value"]] <- as.matrix(as.numeric(as.factor(my_knn[['value']])))
+  pred <- dist_pair %*% my_knn[["value"]]
+  pred <- pred / as.numeric(my_knn["k"])
+  for(i in 1:nrow(pred)){
+    if(as.numeric(pred[i])-1 > 2-as.numeric(pred[i])){
+      pred[i] = 'M'
+    }else{
+      pred[i] = 'B'
+    }
+  }
+  total = 0
+  for (i in 1:nrow(dataset)) {
+    if(dataset[i,1] == pred[i]){
+      total = total + 1
+    }
+  }
+  print("Accuracy")
+  print((total/nrow(dataset))*100)
 }
+
+knn_class = my_knn_regressor(dataset[2:31], dataset[1], k=5)
+predict(knn_class, dataset[2:31])
